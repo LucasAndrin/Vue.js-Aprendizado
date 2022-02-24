8052481667ffdae40d0bd22f6aee253eee647623
LucasAndrin <antmarer@gmail.com>
2022-02-24 17:02:51 -0300

    Primeiro projeto usando componentes externos

---
+++ first-vue-2/src/components/NewComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Olá{{NomeSobrenome}}!</h1>
+        Nome: <input type="text" v-model="nome">
+        Sobrenome: <input type="text" v-model="sobrenome">
+        <button type="button" @click="clear">Limpar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    // retorna um objeto javascript que contém as informações
+    data: function(){
+        return {
+            nome: '',
+            sobrenome: '',
+        }
+    },
+    methods: {
+        clear(){
+            this.nome = '';
+            this.sobrenome = '';
+        }
+    },
+
+    computed: {
+        NomeSobrenome: function(){
+            return ' ' + this.nome + ' ' + this.sobrenome;
+        }
+    }
+}
+</script>
