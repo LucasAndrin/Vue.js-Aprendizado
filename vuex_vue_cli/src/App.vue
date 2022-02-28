805b9120bff8bef7591fef3aadc1302590c9da9e
LucasAndrin <antmarer@gmail.com>
2022-02-28 14:39:06 -0300

    Aprendizado de Vuex com Cli

---
+++ vuex_vue_cli/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" class="col-md-6 mt-5 p-3 bg-light rounded shadow-sm mx-auto">
+    <div class="text-center">
+      <img alt="Vue logo" src="./assets/logo.png">
+      <p>Number: {{getNumber}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  computed: {
+    getNumber: function() {
+      return this.$store.state.number
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
